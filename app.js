9883ef6ac097c5f6187b3e54a6321507badd01ce
Edga380 <a.edgaras44@gmail.com>
2023-11-29 12:06:35 +0000

    Chat bot configured and tested
    Relates #2

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
-  apiKey: process.env.apiKey,
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
 });
 
-const main = async () => {
+const main = async (userMessage, message) => {
   const response = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: "" },
-      { role: "user", content: "" },
-    ],
     model: "gpt-3.5-turbo",
+    messages: userMessage.some(
+      (user) => user.username === message.author.username
+    )
+      ? userMessage.find((user) => user.username === message.author.username)
+          .conversation
+      : [{ role: "user", content: `${message.content}` }],
+    max_tokens: 400, // 400 limit is set because Discord has limit on the reply length that is (2000)
     temperature: 1,
     // stream: true
   });
-
+  // Update userMessage with the system message from GPT
+  if (userMessage.some((user) => user.username === message.author.username)) {
+    const userIndex = userMessage.findIndex(
+      (user) => user.username === message.author.username
+    );
+    userMessage[userIndex].conversation.push({
+      role: `system`,
+      content: `${response.choices[0].message.content}`,
+    });
+  }
+  message.reply(
+    `[${message.author.globalName}] - ${response.choices[0].message.content}`
+  ); // Console log response
   console.log(response.choices);
+  return;
 };
 
-main();
+// Export main()
+module.exports = main;
